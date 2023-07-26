bf973410b3e9dee1107c3dcff894c68d2151a6c4
d-pettersson <david.pettersson@code.berlin>
2023-07-26 15:07:55 +0200

    Remove return value from CoinFlipper

--- CoinFlip/contracts/coinFlipper.sol
+++ CoinFlip/contracts/coinFlipper.sol
@@ -7,11 +7,10 @@ contract CoinFlipper {
     CoinFlip public coinFlip = CoinFlip(0x46A52996B4e73cf21fcC9E4527B7d5D1ccb5BA99);
     uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
-    function flipToWin() public returns (uint) {
+    function flipToWin() public {
         uint256 blockValue = uint256(blockhash(block.number - 1));
         uint256 flip = blockValue / FACTOR;
         bool side = flip == 1 ? true : false;
         coinFlip.flip(side);
-        return coinFlip.consecutiveWins();
     }
 }
